44f9f4349223275e362224ec0975932bf95aeb2d
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-26 15:28:43 -0700

    Add search capabiltiy

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class PolyTreeNode
   def initialize(value, children = [])
     @value = value
@@ -6,7 +7,7 @@ class PolyTreeNode
   end
 
   def parent
-    @parent 
+    @parent
   end
 
   def children
@@ -17,9 +18,87 @@ class PolyTreeNode
     @value
   end
 
-  def parent=(node)
-    node.push(self)
-    @parent = node
+  def push(node)
+    @children << node
   end
 
+  def pop
+    @children.pop
+  end
+
+  def inspect
+     "value: #{self.value} children: #{self.children} parent: #{self.parent}"
+  end
+
+  def parent=(node)
+    if node == nil
+      @parent = node
+      node.children.push(self) unless node == nil
+    end
+    if @parent == nil
+      #debugger
+      @parent = node
+      node.children.push(self) unless node == nil
+    else
+      if !node.children.include?(self)
+        temp = @parent
+        @parent = node
+        node.children.push(self)
+        temp.children.delete(self)
+      end
+    end
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    child_node.parent = nil
+    raise "Not a child of this node" if !self.children.include?(child_node)
+  end
+
+  def dfs(target_value)
+    return nil if self == nil
+    if self.value == target_value
+      return self
+    end
+    stack = self.children
+    stack.each do |child|
+      return child.dfs(target_value) unless child.dfs(target_value) == nil
+    end
+    nil
+    # stack = [self]
+    # until stack.empty?
+    #   el = stack.pop
+    #
+    #   if el.value == target_value
+    #     return el
+    #   end
+    #   el.children.each do |child|
+    #     stack.push(child)
+    #   end
+    # end
+    # nil
+      # if @value == target_value
+      #   return self
+      # end
+  end
+
+  def bfs(target_value)
+
+    queue = [self]
+    until queue.empty?
+      el = queue.shift
+      if el.value == target_value
+        return el
+      end
+      el.children.each do |child|
+        queue.push(child)
+      end
+
+    end
+    nil
+
+  end
 end
